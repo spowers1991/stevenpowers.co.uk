a898cb16305d00deb56f516628196a3d30539fd6
spowers1991 <stevenpowers1991@gmail.com>
2021-11-10 12:20:15 +0000

    workabout v3

--- src/components/blocks/simple.js
+++ src/components/blocks/simple.js
@@ -12,7 +12,34 @@ const Simple = ({ blok }) => {
                 <p className="lead text-grey text-sm lg:text-base font-medium max-w-4xl">
                     {blok.content}
                 </p>
-            </div>      
+            </div>
+            <form className="bg-white shadow-md  px-8 pt-6 pb-8 mb-4 hidden" name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="contact" value="contact" />
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Name
+                    </label>
+                    <input className=" appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Your full name" name="fullname" required />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        email
+                    </label>
+                    <input className=" appearance-none border border-red-500 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="example@gmail.co.uk" name="email" required />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="message">
+                        Message
+                    </label>
+                    <textarea className=" appearance-none border border-red-500 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="message" type="textarea" name="message" placeholder="..." required />
+                    <p className="text-red-500 text-xs italic"></p>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="submit-button bg-black text-white font-bold px-5 py-3 lg:py-3 focus:outline-none uppercase transition transition-gpu duration-100" type="submit">
+                        Send
+                    </button>
+                </div>
+            </form>      
         </SbEditable>
     )
 }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -9,7 +9,6 @@ const Header = () => {
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800&amp;display=swap" rel="stylesheet" />
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
         </Helmet>
-        <input type="hidden" name="contact" value="contact" />
         <nav className="flex items-center justify-between flex-wrap font-inter mt-3 mb-14">
             <div className="block ml-auto">
                 <div className="text-sm lg:flex-grow">
@@ -24,33 +23,6 @@ const Header = () => {
                 </div>
             </div>
         </nav>
-        <form className="bg-white shadow-md  px-8 pt-6 pb-8 mb-4 hidden" name="contact" method="POST" data-netlify="true">
-                <input type="hidden" name="contact" value="contact" />
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
-                            Name
-                        </label>
-                        <input className=" appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Your full name" name="fullname" required />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
-                            email
-                        </label>
-                        <input className=" appearance-none border border-red-500 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="example@gmail.co.uk" name="email" required />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" for="message">
-                            Message
-                        </label>
-                        <textarea className=" appearance-none border border-red-500 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="message" type="textarea" name="message" placeholder="..." required />
-                        <p className="text-red-500 text-xs italic"></p>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button className="submit-button bg-black text-white font-bold px-5 py-3 lg:py-3 focus:outline-none uppercase transition transition-gpu duration-100" type="submit">
-                            Send
-                        </button>
-                    </div>
-                </form>
     </header>
   )
 }
