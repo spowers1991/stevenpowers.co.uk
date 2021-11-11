ff40ecb817f131e5de1f5bbbfccd15ec4f49ada1
spowers1991 <stevenpowers1991@gmail.com>
2021-11-11 22:41:27 +0000

    added links

--- src/components/blocks/IconGrid.js
+++ src/components/blocks/IconGrid.js
@@ -15,13 +15,13 @@ const IconGrid = ({ blok }) => {
                         blok.items.map((item, index) => {
                             return (
                             <div key={index}>
-                                <img className="icon w-3/4 lg:m-0 shadow-md hover:shadow-xl transition transform-gpu duration-100 cursor-pointer  p-5 bg-white mx-auto md-mx-0 z-20" src={item.image.filename} />
+                                <img className="icon w-3/4 lg:m-0 shadow-md hover:shadow-xl transition transform-gpu duration-100 cursor-pointer  p-5 bg-white mx-auto md-mx-0 z-20" src={item.image.filename} alt="" />
                                 <div className="fixed top-0 left-0 right-0 bottom-0 w-3/4 h-3/4 lg:w-1/4 lg:h-3/4 pt-20 pb-17 m-auto modal opacity-0 z-20 bg-white shadow-2xl scale-0 transition transform-gpu duration-100">
                                     <div className="absolute w-full h-7 bg-black top-0 left-0"></div>
                                     <div className="absolute top-1 right-3">
                                          <i className="close-btn far fa-window-close text-white text-xl fas fa-times-circle cursor-pointer hover:text-red"></i>                                 
                                     </div>
-                                    <img className="h-1/4 lg:h-1/2 w-full lg:m-0 m-auto" src={item.image.filename} />
+                                    <img className="h-1/4 lg:h-1/2 w-full lg:m-0 m-auto" src={item.image.filename} alt="" />
                                     <div className="pt-5 px-5 m-auto">
                                         <h3 className="text-2xl lg:text-4xl font-bold font-inter mb-5 border-dashed pb-4 border-b-4 border-light-blue-500 ">
                                             {item.title}
@@ -29,7 +29,7 @@ const IconGrid = ({ blok }) => {
                                         <div className="text-sm lg:text-base">
                                             <RichText>{item.content}</RichText>
                                         </div>
-                                        <a href={`${item.external_Link}`} target="_blank">
+                                        <a href={`${item.external_link}`} target="_blank" rel="noopener noreferrer">
                                             <div className="bg-black text-white p-2 lg:p-5 mt-5 text-center font-inter font-bold uppercase mb-5 relative group text-sm lg:text-base">
                                                 View docs
                                                 <div class="button-bar absolute bottom-0 left-0 right-0 m-auto w-10 max-w-full bg-blue h-1 transform group-hover:w-full transition transition-gpu duration-200"></div>
